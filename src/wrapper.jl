758d57d5da86714c38d172c12be038934df4d3a1
Robert Schwarz <mail@rschwarz.net>
2016-06-19 11:25:21 +0200

    wrapper.jl: some modifications
     - replace `CSIP` prefix with `_`
     - replace all typedefs with their basic C types
     - break lines

--- src/wrapper.jl
+++ src/wrapper.jl
@@ -1,107 +1,149 @@
-# Julia wrapper for header: csip.h
-# Automatically generated using Clang.jl wrap_c, version 0.0.0
-
-
-function CSIPcreateModel(model::Ptr{Ptr{CSIP_MODEL}})
-    ccall((:CSIPcreateModel,csip),CSIP_RETCODE,(Ptr{Ptr{CSIP_MODEL}},),model)
+function _createModel(model::Ptr{Ptr{Void}})
+    ccall((:CSIPcreateModel, csip), Cint, (Ptr{Ptr{Void}}, ), model)
 end
 
-function CSIPfreeModel(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPfreeModel,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},),model)
+function _freeModel(model::Ptr{Void})
+    ccall((:CSIPfreeModel, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPaddVar(model::Ptr{CSIP_MODEL},lowerbound::Cdouble,upperbound::Cdouble,vartype::CSIP_VARTYPE,idx::Ptr{Cint})
-    ccall((:CSIPaddVar,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cdouble,Cdouble,CSIP_VARTYPE,Ptr{Cint}),model,lowerbound,upperbound,vartype,idx)
+function _addVar(model::Ptr{Void}, lowerbound::Cdouble, upperbound::Cdouble,
+                 vartype::Cint, idx::Ptr{Cint})
+    ccall((:CSIPaddVar, csip), Cint,
+          (Ptr{Void}, Cdouble, Cdouble, Cint, Ptr{Cint}),
+          model, lowerbound, upperbound, vartype, idx)
 end
 
-function CSIPchgVarLB(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},lowerbounds::Ptr{Cdouble})
-    ccall((:CSIPchgVarLB,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble}),model,numindices,indices,lowerbounds)
+function _chgVarLB(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                   lowerbounds::Ptr{Cdouble})
+    ccall((:CSIPchgVarLB, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}),
+          model, numindices, indices, lowerbounds)
 end
 
-function CSIPchgVarUB(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},upperbounds::Ptr{Cdouble})
-    ccall((:CSIPchgVarUB,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble}),model,numindices,indices,upperbounds)
+function _chgVarUB(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                   upperbounds::Ptr{Cdouble})
+    ccall((:CSIPchgVarUB, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}),
+          model, numindices, indices, upperbounds)
 end
 
-function CSIPaddLinCons(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},coefs::Ptr{Cdouble},lhs::Cdouble,rhs::Cdouble,idx::Ptr{Cint})
-    ccall((:CSIPaddLinCons,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble},Cdouble,Cdouble,Ptr{Cint}),model,numindices,indices,coefs,lhs,rhs,idx)
+function _addLinCons(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                     coefs::Ptr{Cdouble}, lhs::Cdouble, rhs::Cdouble,
+                     idx::Ptr{Cint})
+    ccall((:CSIPaddLinCons, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}, Cdouble, Cdouble, Ptr{Cint}),
+          model, numindices, indices, coefs, lhs, rhs, idx)
 end
 
-function CSIPaddQuadCons(model::Ptr{CSIP_MODEL},numlinindices::Cint,linindices::Ptr{Cint},lincoefs::Ptr{Cdouble},numquadterms::Cint,quadrowindices::Ptr{Cint},quadcolindices::Ptr{Cint},quadcoefs::Ptr{Cdouble},lhs::Cdouble,rhs::Cdouble,idx::Ptr{Cint})
-    ccall((:CSIPaddQuadCons,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble},Cint,Ptr{Cint},Ptr{Cint},Ptr{Cdouble},Cdouble,Cdouble,Ptr{Cint}),model,numlinindices,linindices,lincoefs,numquadterms,quadrowindices,quadcolindices,quadcoefs,lhs,rhs,idx)
+function _addQuadCons(model::Ptr{Void}, numlinindices::Cint,
+                      linindices::Ptr{Cint}, lincoefs::Ptr{Cdouble},
+                      numquadterms::Cint, quadrowindices::Ptr{Cint},
+                      quadcolindices::Ptr{Cint}, quadcoefs::Ptr{Cdouble},
+                      lhs::Cdouble, rhs::Cdouble, idx::Ptr{Cint})
+    ccall((:CSIPaddQuadCons, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}, Cint, Ptr{Cint}, Ptr{Cint},
+           Ptr{Cdouble}, Cdouble, Cdouble, Ptr{Cint}),
+          model, numlinindices, linindices, lincoefs, numquadterms,
+          quadrowindices, quadcolindices, quadcoefs, lhs, rhs, idx)
 end
 
-function CSIPaddSOS1(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},weights::Ptr{Cdouble},idx::Ptr{Cint})
-    ccall((:CSIPaddSOS1,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble},Ptr{Cint}),model,numindices,indices,weights,idx)
+function _addSOS1(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                  weights::Ptr{Cdouble}, idx::Ptr{Cint})
+    ccall((:CSIPaddSOS1, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint}),
+          model, numindices, indices, weights, idx)
 end
 
-function CSIPaddSOS2(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},weights::Ptr{Cdouble},idx::Ptr{Cint})
-    ccall((:CSIPaddSOS2,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble},Ptr{Cint}),model,numindices,indices,weights,idx)
+function _addSOS2(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                  weights::Ptr{Cdouble}, idx::Ptr{Cint})
+    ccall((:CSIPaddSOS2, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint}),
+          model, numindices, indices, weights, idx)
 end
 
-function CSIPsetObj(model::Ptr{CSIP_MODEL},numindices::Cint,indices::Ptr{Cint},coefs::Ptr{Cdouble})
-    ccall((:CSIPsetObj,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Cint,Ptr{Cint},Ptr{Cdouble}),model,numindices,indices,coefs)
+function _setObj(model::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                 coefs::Ptr{Cdouble})
+    ccall((:CSIPsetObj, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}),
+          model, numindices, indices, coefs)
 end
 
-function CSIPsetSenseMinimize(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPsetSenseMinimize,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},),model)
+function _setSenseMinimize(model::Ptr{Void})
+    ccall((:CSIPsetSenseMinimize, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPsetSenseMaximize(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPsetSenseMaximize,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},),model)
+function _setSenseMaximize(model::Ptr{Void})
+    ccall((:CSIPsetSenseMaximize, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPsolve(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPsolve,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},),model)
+function _solve(model::Ptr{Void})
+    ccall((:CSIPsolve, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPgetVarValues(model::Ptr{CSIP_MODEL},output::Ptr{Cdouble})
-    ccall((:CSIPgetVarValues,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Ptr{Cdouble}),model,output)
+function _getVarValues(model::Ptr{Void}, output::Ptr{Cdouble})
+    ccall((:CSIPgetVarValues, csip), Cint, (Ptr{Void}, Ptr{Cdouble}),
+          model, output)
 end
 
-function CSIPgetObjValue(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPgetObjValue,csip),Cdouble,(Ptr{CSIP_MODEL},),model)
+function _getObjValue(model::Ptr{Void})
+    ccall((:CSIPgetObjValue, csip), Cdouble, (Ptr{Void}, ), model)
 end
 
-function CSIPgetStatus(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPgetStatus,csip),CSIP_STATUS,(Ptr{CSIP_MODEL},),model)
+function _getStatus(model::Ptr{Void})
+    ccall((:CSIPgetStatus, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPsetParameterGeneric(model::Ptr{CSIP_MODEL},name::Ptr{UInt8},value::Ptr{Void})
-    ccall((:CSIPsetParameterGeneric,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Ptr{UInt8},Ptr{Void}),model,name,value)
+function _setParameterGeneric(model::Ptr{Void}, name::Ptr{UInt8},
+                              value::Ptr{Void})
+    ccall((:CSIPsetParameterGeneric, csip), Cint,
+          (Ptr{Void}, Ptr{UInt8}, Ptr{Void}),
+          model, name, value)
 end
 
-function CSIPgetNumVars(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPgetNumVars,csip),Cint,(Ptr{CSIP_MODEL},),model)
+function _getNumVars(model::Ptr{Void})
+    ccall((:CSIPgetNumVars, csip), Cint, (Ptr{Void}, ), model)
 end
 
-function CSIPsetInitialSolution(model::Ptr{CSIP_MODEL},values::Ptr{Cdouble})
-    ccall((:CSIPsetInitialSolution,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},Ptr{Cdouble}),model,values)
+function _setInitialSolution(model::Ptr{Void}, values::Ptr{Cdouble})
+    ccall((:CSIPsetInitialSolution, csip), Cint, (Ptr{Void}, Ptr{Cdouble}),
+          model, values)
 end
 
-function CSIPcbGetVarValues(cbdata::Ptr{CSIP_CBDATA},output::Ptr{Cdouble})
-    ccall((:CSIPcbGetVarValues,csip),CSIP_RETCODE,(Ptr{CSIP_CBDATA},Ptr{Cdouble}),cbdata,output)
+function _cbGetVarValues(cbdata::Ptr{Void}, output::Ptr{Cdouble})
+    ccall((:CSIPcbGetVarValues, csip), Cint, (Ptr{Void}, Ptr{Cdouble}),
+          cbdata, output)
 end
 
-function CSIPcbAddLinCons(cbdata::Ptr{CSIP_CBDATA},numindices::Cint,indices::Ptr{Cint},coefs::Ptr{Cdouble},lhs::Cdouble,rhs::Cdouble,islocal::Cint)
-    ccall((:CSIPcbAddLinCons,csip),CSIP_RETCODE,(Ptr{CSIP_CBDATA},Cint,Ptr{Cint},Ptr{Cdouble},Cdouble,Cdouble,Cint),cbdata,numindices,indices,coefs,lhs,rhs,islocal)
+function _cbAddLinCons(cbdata::Ptr{Void}, numindices::Cint, indices::Ptr{Cint},
+                       coefs::Ptr{Cdouble}, lhs::Cdouble, rhs::Cdouble, islocal::Cint)
+    ccall((:CSIPcbAddLinCons, csip), Cint,
+          (Ptr{Void}, Cint, Ptr{Cint}, Ptr{Cdouble}, Cdouble, Cdouble, Cint),
+          cbdata, numindices, indices, coefs, lhs, rhs, islocal)
 end
 
-function CSIPaddLazyCallback(model::Ptr{CSIP_MODEL},cb::CSIP_LAZYCALLBACK,fractional::Cint,userdata::Ptr{Void})
-    ccall((:CSIPaddLazyCallback,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},CSIP_LAZYCALLBACK,Cint,Ptr{Void}),model,cb,fractional,userdata)
+function _addLazyCallback(model::Ptr{Void}, cb::Void, fractional::Cint,
+                          userdata::Ptr{Void})
+    ccall((:CSIPaddLazyCallback, csip), Cint,
+          (Ptr{Void}, Void, Cint, Ptr{Void}),
+          model, cb, fractional, userdata)
 end
 
-function CSIPheurGetVarValues(heurdata::Ptr{CSIP_HEURDATA},output::Ptr{Cdouble})
-    ccall((:CSIPheurGetVarValues,csip),CSIP_RETCODE,(Ptr{CSIP_HEURDATA},Ptr{Cdouble}),heurdata,output)
+function _heurGetVarValues(heurdata::Ptr{Void}, output::Ptr{Cdouble})
+    ccall((:CSIPheurGetVarValues, csip), Cint, (Ptr{Void}, Ptr{Cdouble}),
+          heurdata, output)
 end
 
-function CSIPheurSetSolution(heurdata::Ptr{CSIP_HEURDATA},values::Ptr{Cdouble})
-    ccall((:CSIPheurSetSolution,csip),CSIP_RETCODE,(Ptr{CSIP_HEURDATA},Ptr{Cdouble}),heurdata,values)
+function _heurSetSolution(heurdata::Ptr{Void}, values::Ptr{Cdouble})
+    ccall((:CSIPheurSetSolution, csip), Cint, (Ptr{Void}, Ptr{Cdouble}),
+          heurdata, values)
 end
 
-function CSIPaddHeuristicCallback(model::Ptr{CSIP_MODEL},heur::CSIP_HEURCALLBACK,userdata::Ptr{Void})
-    ccall((:CSIPaddHeuristicCallback,csip),CSIP_RETCODE,(Ptr{CSIP_MODEL},CSIP_HEURCALLBACK,Ptr{Void}),model,heur,userdata)
+function _addHeuristicCallback(model::Ptr{Void}, heur::Void,
+                               userdata::Ptr{Void})
+    ccall((:CSIPaddHeuristicCallback, csip), Cint, (Ptr{Void}, Void, Ptr{Void}),
+          model, heur, userdata)
 end
 
-function CSIPgetInternalSCIP(model::Ptr{CSIP_MODEL})
-    ccall((:CSIPgetInternalSCIP,csip),Ptr{Void},(Ptr{CSIP_MODEL},),model)
+function _getInternalSCIP(model::Ptr{Void})
+    ccall((:CSIPgetInternalSCIP, csip), Ptr{Void}, (Ptr{Void}, ), model)
 end
