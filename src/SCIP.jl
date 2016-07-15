0a6ece50f277591d8a1708fe68dd92976b05714a
Robert Schwarz <mail@rschwarz.net>
2016-07-15 21:57:20 +0200

    setup deps.jl

--- src/SCIP.jl
+++ src/SCIP.jl
@@ -1,5 +1,14 @@
 module SCIP
 
-import MathProgBase
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("SCIP.jl not properly installed. Please run Pkg.build(\"SCIP\")")
+end
+
+
+importall MathProgBase.SolverInterface
+
+include("wrapper.jl")
 
 end
