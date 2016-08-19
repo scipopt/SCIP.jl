3e6aa5b68a95494910e792a1fc96adbb673563fb
Miles Lubin <miles.lubin@gmail.com>
2016-08-19 10:19:30 -0600

    run JuMP's tests with SCIP

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,17 @@ using MathProgBase
 using FactCheck
 
 include("mixintprog.jl")
+
+include(joinpath(Pkg.dir("JuMP"),"test","solvers.jl"))
+
+using JuMP
+solver = SCIPSolver()
+lp_solvers = lp_solvers[1:1] # just need one
+ip_solvers = [solver]
+quad_solvers = []
+quad_mip_solvers = [solver]
+include(joinpath(Pkg.dir("JuMP"),"test","model.jl"))
+include(joinpath(Pkg.dir("JuMP"),"test","probmod.jl"))
+include(joinpath(Pkg.dir("JuMP"),"test","qcqpmodel.jl"))
+#include(joinpath(Pkg.dir("JuMP"),"test","nonlinear.jl"))
+#include(joinpath(Pkg.dir("JuMP"),"test","sdp.jl"))
