c7c1a7a00e347df4b357c006c377406412512a80
Miles Lubin <miles.lubin@gmail.com>
2016-08-19 10:28:21 -0600

    commented-out callback test

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,8 +12,10 @@ lp_solvers = lp_solvers[1:1] # just need one
 ip_solvers = [solver]
 quad_solvers = []
 quad_mip_solvers = [solver]
+lazy_solvers = [solver]
 include(joinpath(Pkg.dir("JuMP"),"test","model.jl"))
 include(joinpath(Pkg.dir("JuMP"),"test","probmod.jl"))
 include(joinpath(Pkg.dir("JuMP"),"test","qcqpmodel.jl"))
+#include(joinpath(Pkg.dir("JuMP"),"test","callback.jl"))
 #include(joinpath(Pkg.dir("JuMP"),"test","nonlinear.jl"))
 #include(joinpath(Pkg.dir("JuMP"),"test","sdp.jl"))
