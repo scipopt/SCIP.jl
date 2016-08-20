ad7d18618f9ec4264ea17f118ef86eac814d1c6e
Robert Schwarz <mail@rschwarz.net>
2016-08-20 16:22:07 +0200

    disable quadratic tests
     - they were still failing because we don't support quadratic objectives

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,11 +11,12 @@ solver = SCIPSolver("display/verblevel", 0)
 lp_solvers = lp_solvers[1:1] # just need one
 ip_solvers = [solver]
 quad_solvers = []
-quad_mip_solvers = [solver]
+quad_mip_solvers = []
+#quad_mip_solvers = [solver]
 lazy_solvers = [solver]
 include(joinpath(Pkg.dir("JuMP"),"test","model.jl"))
 include(joinpath(Pkg.dir("JuMP"),"test","probmod.jl"))
-include(joinpath(Pkg.dir("JuMP"),"test","qcqpmodel.jl"))
+#include(joinpath(Pkg.dir("JuMP"),"test","qcqpmodel.jl"))
 #include(joinpath(Pkg.dir("JuMP"),"test","callback.jl"))
 #include(joinpath(Pkg.dir("JuMP"),"test","nonlinear.jl"))
 #include(joinpath(Pkg.dir("JuMP"),"test","sdp.jl"))
