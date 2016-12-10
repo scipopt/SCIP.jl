a4707b7947e29a02098b38c77ce55b81a39a24c6
Robert Schwarz <mail@rschwarz.net>
2016-12-10 21:19:50 +0100

    use Base.Test instead of FactCheck

--- test/mixintprog.jl
+++ test/mixintprog.jl
@@ -1,9 +1,9 @@
-facts("solving MIP with mixintprog") do
+@testset "solving MIP with mixintprog" begin
     solver = SCIPSolver("display/verblevel", 0)
 
     # integer knapsack problem
     sol = mixintprog(-[5,3,2,7,4],Float64[2 8 4 2 5],'<',10,:Int,0,1,solver)
-    @fact sol.status --> :Optimal
-    @fact sol.objval --> roughly(-16.0)
-    @fact sol.sol --> roughly([1.0, 0.0, 0.0, 1.0, 1.0])
+    @test sol.status == :Optimal
+    @test sol.objval ≈ -16.0
+    @test sol.sol ≈ [1.0, 0.0, 0.0, 1.0, 1.0]
 end
