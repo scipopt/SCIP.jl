ab5119ddf1272254b507c428195640262ae162ab
Robert Schwarz <mail@rschwarz.net>
2017-08-10 20:53:39 +0200

    update CSIP version (for bugfix there)

--- deps/csip_version.jl
+++ deps/csip_version.jl
@@ -1,5 +1,5 @@
 # required version and utilities in a single location
-CSIP_VERSION = "0.4.2"
+CSIP_VERSION = "0.4.3"
 
 function vn2int(vn::VersionNumber)
     100*vn.major + 10*vn.minor + vn.patch
