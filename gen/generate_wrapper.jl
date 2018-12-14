479aae1dd0690bc91fed38a303d6e6890a862f76
Robert Schwarz <mail@rschwarz.net>
2018-12-14 23:55:10 +0100

    don't add pub_*.h headers (for now)

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -22,6 +22,7 @@ top_types = [
 headers = vcat(
     filter(h -> h != "type_var.h", top_types), # problem: `union SCIP_DomChg`
     filter(h -> startswith(h, "type_") && !in(h, top_types), all_headers),
+    # filter(h -> startswith(h, "pub_"), all_headers),
     filter(h -> startswith(h, "scip_"), all_headers),
 )
 clang_includes = [
