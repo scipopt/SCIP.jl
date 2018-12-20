49bbe949c1c16752b9d1f5ad0872e71faee57e4c
Robert Schwarz <mail@rschwarz.net>
2018-12-20 14:29:00 +0100

    manage vars, conss for ManagedSCIP

--- src/managed_scip.jl
+++ src/managed_scip.jl
@@ -23,10 +23,47 @@ end
 function free_scip(mscip::ManagedSCIP)
     # avoid double-free
     if mscip.scip[] != C_NULL
-        # TODO: release all constraints
-        # TODO: release all variables
-        rc = SCIP.SCIPfree(mscip.scip)
+        for cons in mscip.conss
+            rc = SCIPreleaseCons(mscip.scip[], cons)
+            @assert rc == SCIP_OKAY
+        end
+        for var in mscip.vars
+            rc = SCIPreleaseVar(mscip.scip[], var)
+            @assert rc == SCIP_OKAY
+        end
+        rc = SCIPfree(mscip.scip)
         @assert rc == SCIP_OKAY
     end
     @assert mscip.scip[] == C_NULL
+end
+
+"Add variable to problem (continuous, no bounds)"
+function add_variable(mscip::ManagedSCIP)
+    var = Ref{Ptr{SCIP_VAR}}()
+    rc = SCIPcreateVarBasic(
+        mscip.scip[], var, "", -SCIPinfinity(mscip.scip[]),
+        SCIPinfinity(mscip.scip[]), 0.0, SCIP_VARTYPE_CONTINUOUS)
+    @assert rc == SCIP_OKAY
+    rc = SCIPaddVar(mscip.scip[], var[])
+    @assert rc == SCIP_OKAY
+
+    push!(mscip.vars, var)
+    # can't delete variable, so we use the array position as index
+    return length(mscip.vars)
+end
+
+"Add (ranged) linear constraint to problem"
+function add_linear_constraint(mscip::ManagedSCIP, varidx, coeffs, lhs, rhs)
+    @assert length(varidx) == length(coeffs)
+    vars = [mscip.vars[i][] for i in varidx]
+    cons = Ref{Ptr{SCIP_CONS}}()
+    rc = SCIPcreateConsBasicLinear(
+        mscip.scip[], cons, "", length(vars), vars, coeffs, lhs, rhs)
+    @assert rc == SCIP_OKAY
+    rc = SCIPaddCons(mscip.scip[], cons[])
+    @assert rc == SCIP_OKAY
+
+    push!(mscip.conss, cons)
+    # can't delete constraint, so we use the array position as index
+    return length(mscip.conss)
 end
--- test/managed_scip.jl
+++ test/managed_scip.jl
@@ -4,6 +4,14 @@
     mscip = SCIP.ManagedSCIP()
     @test mscip.scip[] != C_NULL
     SCIP.free_scip(mscip)
+    @test mscip.scip[] == C_NULL
+end
+
+@testset "create and semi-manual free" begin
+    mscip = SCIP.ManagedSCIP()
+    @test mscip.scip[] != C_NULL
+    finalize(mscip)
+    @test mscip.scip[] == C_NULL
 end
 
 @testset "create and implicit free" begin
@@ -16,4 +24,22 @@ end
     GC.gc()
     GC.gc() # TODO: why need a second call?
     @test ptr[] == C_NULL
+end
+
+@testset "create with vars and cons, (no solve), and free" begin
+    mscip = SCIP.ManagedSCIP()
+    @test mscip.scip[] != C_NULL
+
+    x = SCIP.add_variable(mscip)
+    y = SCIP.add_variable(mscip)
+    c = SCIP.add_linear_constraint(mscip, [x, y], [2.0, 3.0], 1.0, 9.0)
+
+    finalize(mscip)
+    for var in mscip.vars
+        @test var[] == C_NULL
+    end
+    for cons in mscip.conss
+        @test cons[] == C_NULL
+    end
+    @test mscip.scip[] == C_NULL
 end
