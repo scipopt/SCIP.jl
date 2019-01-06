04e31fe03ae776a70991d0db7c675b8f9a9d777d
Robert Schwarz <mail@rschwarz.net>
2019-01-06 14:50:05 +0100

    rm test that depends on GC behavior

--- test/managed_scip.jl
+++ test/managed_scip.jl
@@ -14,18 +14,6 @@ end
     @test mscip.scip[] == C_NULL
 end
 
-@testset "create and implicit free" begin
-    ptr = Ref{Ptr{SCIP.SCIP_}}(C_NULL)
-    for i = 1:1
-        mscip = SCIP.ManagedSCIP()
-        ptr = mscip.scip
-        @test ptr[] != C_NULL
-    end
-    GC.gc()
-    GC.gc() # TODO: why need a second call?
-    @test ptr[] == C_NULL
-end
-
 @testset "create with vars and cons, (no solve), and free" begin
     mscip = SCIP.ManagedSCIP()
     @test mscip.scip[] != C_NULL
