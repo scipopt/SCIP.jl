4cb2635f1e5e57bbc6660ee67e0ba1cfa1b4ddfc
Robert Schwarz <mail@rschwarz.net>
2019-01-09 14:40:07 +0100

    implement some MOI methods for ScalarQuadraticFunction

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -8,12 +8,14 @@ const CI = MOI.ConstraintIndex
 # supported functions
 const SVF = MOI.SingleVariable
 const SAF = MOI.ScalarAffineFunction{Float64}
+const SQF = MOI.ScalarQuadraticFunction{Float64}
 # supported sets
 const BOUNDS = Union{MOI.EqualTo{Float64}, MOI.GreaterThan{Float64},
                      MOI.LessThan{Float64}, MOI.Interval{Float64}}
 const VAR_TYPES = Union{MOI.ZeroOne, MOI.Integer}
 # other MOI types
 const AFF_TERM = MOI.ScalarAffineTerm{Float64}
+const QUAD_TERM = MOI.ScalarQuadraticTerm{Float64}
 
 const PtrMap = Dict{Ptr{Cvoid}, Union{VarRef, ConsRef}}
 const ConsTypeMap = Dict{Tuple{DataType, DataType}, Vector{ConsRef}}
@@ -90,6 +92,8 @@ MOI.supports_constraint(o::Optimizer, ::Type{SVF}, ::Type{<:BOUNDS}) = true
 MOI.supports_constraint(o::Optimizer, ::Type{SVF}, ::Type{<:VAR_TYPES}) = true
 # linear constraints
 MOI.supports_constraint(o::Optimizer, ::Type{SAF}, ::Type{<:BOUNDS}) = true
+# quadratic constraints
+MOI.supports_constraint(o::Optimizer, ::Type{SQF}, ::Type{<:BOUNDS}) = true
 
 MOI.supports(::Optimizer, ::MOI.ObjectiveSense) = true
 MOI.supports(::Optimizer, ::MOI.ObjectiveFunction{SAF}) = true
@@ -260,8 +264,52 @@ function MOI.set(o::SCIP.Optimizer, ::MOI.ConstraintSet, ci::CI{SAF,S}, set::S) 
     return nothing
 end
 
-function MOI.is_valid(o::Optimizer, ci::CI{SAF,<:BOUNDS})
-    return 1 <= ci.value <= length(o.mscip.cons)
+function MOI.add_constraint(o::Optimizer, func::SQF, set::S) where {S <: BOUNDS}
+    if func.constant != 0.0
+        error("SCIP does not support quadratic constraints with a constant offset.")
+    end
+
+    allow_modification(o)
+
+    # affine terms
+    linrefs = [VarRef(t.variable_index.value) for t in func.affine_terms]
+    lincoefs = [t.coefficient for t in func.affine_terms]
+
+    # quadratic terms
+    quadrefs1 = [VarRef(t.variable_index_1.value) for t in func.quadratic_terms]
+    quadrefs2 = [VarRef(t.variable_index_2.value) for t in func.quadratic_terms]
+    quadcoefs = [t.coefficient for t in func.quadratic_terms]
+
+    # range
+    lhs, rhs = bounds(set)
+    lhs = lhs == nothing ? -SCIPinfinity(scip(o)) : lhs
+    rhs = rhs == nothing ?  SCIPinfinity(scip(o)) : rhs
+
+    cr = add_quadratic_constraint(o.mscip, linrefs, lincoefs,
+                                  quadrefs1, quadrefs2, quadcoefs, lhs, rhs)
+    ci = CI{SQF, S}(cr.val)
+    register!(o, ci)
+    register!(o, cons(o, ci), cr)
+    return ci
+end
+
+function MOI.set(o::SCIP.Optimizer, ::MOI.ConstraintSet, ci::CI{SQF,S}, set::S) where {S <: BOUNDS}
+    allow_modification(o)
+
+    lhs, rhs = bounds(set)
+    lhs = lhs == nothing ? -SCIPinfinity(scip(o)) : lhs
+    rhs = rhs == nothing ?  SCIPinfinity(scip(o)) : rhs
+
+    @SC SCIPchgLhsQuadratic(scip(o), cons(o, ci), lhs)
+    @SC SCIPchgRhsQuadratic(scip(o), cons(o, ci), rhs)
+
+    return nothing
+end
+
+function MOI.get(o::Optimizer, ::MOI.ConstraintSet, ci::CI{SQF, S}) where S <: BOUNDS
+    lhs = SCIPgetLhsQuadratic(scip(o), cons(o, ci))
+    rhs = SCIPgetRhsQuadratic(scip(o), cons(o, ci))
+    return from_bounds(S, lhs, rhs)
 end
 
 function MOI.get(o::Optimizer, ::MOI.NumberOfConstraints{F,S}) where {F,S}
