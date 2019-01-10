138a6469662a5f276d1774e1a25446657322006f
Robert Schwarz <mail@rschwarz.net>
2019-01-10 13:38:31 +0100

    merge to tests

--- test/managed_scip.jl
+++ test/managed_scip.jl
@@ -14,45 +14,29 @@ end
     @test mscip.scip[] == C_NULL
 end
 
-@testset "create with vars and cons, (no solve), and free" begin
-    mscip = SCIP.ManagedSCIP()
-    @test mscip.scip[] != C_NULL
+@testset "create with vars and cons, and free" begin
+    for i=1:2 # run twice, with(out) solving
+        mscip = SCIP.ManagedSCIP()
+        @test mscip.scip[] != C_NULL
+        SCIP.set_parameter(mscip, "display/verblevel", 0)
 
-    x = SCIP.add_variable(mscip)
-    y = SCIP.add_variable(mscip)
-    c = SCIP.add_linear_constraint(mscip, [x, y], [2.0, 3.0], 1.0, 9.0)
-    q = SCIP.add_quadratic_constraint(mscip, [x], [2.0], [x, x], [x, y], [4.0, 5.0], 1.0, 9.0)
+        x = SCIP.add_variable(mscip)
+        y = SCIP.add_variable(mscip)
+        c = SCIP.add_linear_constraint(mscip, [x, y], [2.0, 3.0], 1.0, 9.0)
+        q = SCIP.add_quadratic_constraint(mscip, [x], [2.0], [x, x], [x, y], [4.0, 5.0], 1.0, 9.0)
 
-    finalize(mscip)
-    for var in mscip.vars
-        @test var[] == C_NULL
+        if i==2
+            # solve, but don't check results (this test is about memory mgmt)
+            SCIP.@SC SCIP.SCIPsolve(mscip.scip[])
+        end
+
+        finalize(mscip)
+        for var in mscip.vars
+            @test var[] == C_NULL
+        end
+        for cons in mscip.conss
+            @test cons[] == C_NULL
+        end
+        @test mscip.scip[] == C_NULL
     end
-    for cons in mscip.conss
-        @test cons[] == C_NULL
-    end
-    @test mscip.scip[] == C_NULL
-end
-
-@testset "create with vars and cons, (no solve), and free" begin
-    mscip = SCIP.ManagedSCIP()
-    @test mscip.scip[] != C_NULL
-
-    SCIP.set_parameter(mscip, "display/verblevel", 0)
-
-    x = SCIP.add_variable(mscip)
-    y = SCIP.add_variable(mscip)
-    c = SCIP.add_linear_constraint(mscip, [x, y], [2.0, 3.0], 1.0, 9.0)
-    q = SCIP.add_quadratic_constraint(mscip, [x], [2.0], [x, x], [x, y], [4.0, 5.0], 1.0, 9.0)
-
-    # solve, but don't check results (this test is about memory mgmt)
-    SCIP.@SC SCIP.SCIPsolve(mscip.scip[])
-
-    finalize(mscip)
-    for var in mscip.vars
-        @test var[] == C_NULL
-    end
-    for cons in mscip.conss
-        @test cons[] == C_NULL
-    end
-    @test mscip.scip[] == C_NULL
 end
