a15f8b7c2dbfc0c31fe5090facd418cf5b2c11b2
Robert Schwarz <mail@rschwarz.net>
2019-01-11 21:05:07 +0100

    add init.jl

--- src/SCIP.jl
+++ src/SCIP.jl
@@ -1,24 +1,7 @@
 module SCIP
 
-import Libdl
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("SCIP was not built properly, please run Pkg.build(\"SCIP\")")
-end
-include(depsjl_path)
-
-function __init__()
-    major = SCIPmajorVersion()
-    minor = SCIPminorVersion()
-    patch = SCIPtechVersion()
-    current = VersionNumber("$major.$minor.$patch")
-    required = VersionNumber("6.0.0")
-    upperbound = VersionNumber(required.major + 1)
-    if current < required || current >= upperbound
-        error("SCIP is installed at version $current, " *
-              "supported are $required up to $upperbound.")
-    end
-end
+# load deps, version check
+include("init.jl")
 
 # wrapper of SCIP library
 include("wrapper.jl")
