152e2f9b1a1459855d80b660bb014ec949fe1005
Robert Schwarz <mail@rschwarz.net>
2019-02-22 22:00:09 +0100

    add test with various expression types

--- test/MOI_nonlinear_exprs.jl
+++ test/MOI_nonlinear_exprs.jl
@@ -54,3 +54,54 @@ MOI.constraint_expr(evaluator::ExprEvaluator, i) = evaluator.constraints[i]
     @test MOI.get(optimizer, MOI.VariablePrimal(), x) ≈ 0.75 atol=atol rtol=rtol
     @test MOI.get(optimizer, MOI.VariablePrimal(), y) ≈ 0.0 atol=atol rtol=rtol
 end
+
+@testset "pot pourri" begin
+    optimizer = SCIP.Optimizer(display_verblevel=0)
+
+    @test MOI.supports(optimizer, MOI.NLPBlock()) == true
+
+    num_vars = 17
+    vars = MOI.add_variables(optimizer, num_vars)
+    for i in 1:num_vars
+        MOI.add_constraint(optimizer, vars[i], MOI.Interval(0.1, 10.0))
+    end
+
+    # Nonlinear equations.
+    expressions = Expr[
+        :(x[1] + 0.5),          # VARIDX / CONSTANT
+        :(x[2] - x[3]),         # MINUS (binary)
+      # :(-x[4]),               # MINUS (unary)
+        :(x[5] + x[6] + x[7]),  # SUM
+        :(x[8] * x[9] * x[10]), # PRODUCT
+        :((x[11] + x[12])^0.8), # REALPOWER
+        :(x[13] / x[14]),       # DIV
+        :(sqrt(x[15])),         # SQRT
+        :(exp(x[16])),          # EXP
+        :(log(x[17])),          # LOG
+    ]
+    rhs = 2.0
+
+    data = MOI.NLPBlockData(
+        [MOI.NLPBoundsPair(rhs, rhs) for i in 1:length(expressions)],
+        ExprEvaluator(expressions),
+        false # no objective
+    )
+    MOI.set(optimizer, MOI.NLPBlock(), data)
+
+    # Solve and get results.
+    MOI.optimize!(optimizer)
+    @test MOI.get(optimizer, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test MOI.get(optimizer, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+    sol = MOI.get(optimizer, MOI.VariablePrimal(), vars)
+
+    # Evaluate lhs of all the expressions with solution values.
+    values = Float64[]
+    for expr in expressions
+        @eval lhs(x) = $expr
+        push!(values, lhs(sol))
+    end
+    references = [rhs  for i in 1:length(expressions)]
+
+    atol, rtol = 1e-6, 1e-6
+    @test values ≈ references atol=atol rtol=rtol
+end
