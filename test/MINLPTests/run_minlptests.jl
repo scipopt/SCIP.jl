b56e97ae25fa94b0301e3842211fd07b7066f389
Robert Schwarz <mail@rschwarz.net>
2019-03-02 20:54:32 +0100

    fix typo in run_minlptests, rm unsupported test
     - operator `\`, is not even supported through JuMP/MOI?

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -5,13 +5,8 @@ const OBJTOL = 1e-5
 const PRIMALTOL = 1e-5
 const DUALTOL = NaN  # to disable the query
 
-@testset "MINLPTests" begin
+@testset "MINLPTests - nlp" begin
     @testset "nlp_005_010" begin
-        MINLPTests.nlp_005_010(OPTIMIZER, OBJTOL, PRIMALTOL, DUALTOL;
-                               termination_target = MOI.OPTIMAL)
-    end
-
-    @testset "nlp_005_011" begin
         MINLPTests.nlp_005_010(OPTIMIZER, OBJTOL, PRIMALTOL, DUALTOL;
                                termination_target = MOI.OPTIMAL)
     end
