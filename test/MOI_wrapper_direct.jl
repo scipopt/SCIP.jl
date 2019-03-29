b4ac7324b3b30caa18e48d79cc728d2d9ca03384
Robert Schwarz <mail@rschwarz.net>
2019-03-29 13:35:20 +0100

    direct solver: call tests individually
     - will not fail when future tests use unsupported constraints or other features
     - as discussed in https://github.com/JuliaOpt/MathOptInterface.jl/pull/697

--- test/MOI_wrapper_direct.jl
+++ test/MOI_wrapper_direct.jl
@@ -7,6 +7,7 @@ const CONFIG = MOIT.TestConfig(atol=1e-5, rtol=1e-5, duals=false,
                                infeas_certificates=false)
 
 @testset "MOI Continuous Linear" begin
+    # remember reason for excluded tests:
     excluded = [
         "linear1",  # needs MOI.delete (of variables in constraints)
         "linear5",  # needs MOI.delete (of variables in constraints)
@@ -17,16 +18,42 @@ const CONFIG = MOIT.TestConfig(atol=1e-5, rtol=1e-5, duals=false,
         "linear15", # needs MOI.VectorAffineFunction
         "partial_start", # TODO: supportVariablePrimalStart
     ]
-    MOIT.contlineartest(OPTIMIZER, CONFIG, excluded)
+    # MOIT.contlineartest(OPTIMIZER, CONFIG, excluded)
+
+    # call individual tests
+    MOIT.linear2test(OPTIMIZER, CONFIG)
+    MOIT.linear3test(OPTIMIZER, CONFIG)
+    MOIT.linear4test(OPTIMIZER, CONFIG)
+    MOIT.linear6test(OPTIMIZER, CONFIG)
+    MOIT.linear8atest(OPTIMIZER, CONFIG)
+    MOIT.linear8btest(OPTIMIZER, CONFIG)
+    MOIT.linear8ctest(OPTIMIZER, CONFIG)
+    MOIT.linear9test(OPTIMIZER, CONFIG)
+    MOIT.linear10test(OPTIMIZER, CONFIG)
+    MOIT.linear10btest(OPTIMIZER, CONFIG)
+    MOIT.linear12test(OPTIMIZER, CONFIG)
 end
 
 @testset "MOI Quadratic Constraint" begin
+    # remember reason for excluded tests:
     excluded = [
         "qcp1", # needs VectorAffineFunction
     ]
-    MOIT.qcptest(OPTIMIZER, CONFIG, excluded)
+    # MOIT.qcptest(OPTIMIZER, CONFIG, excluded)
+
+    # call individual tests
+    MOIT.qcp2test(OPTIMIZER, CONFIG)
+    MOIT.qcp3test(OPTIMIZER, CONFIG)
+    # MOIT.qcp4test(OPTIMIZER, CONFIG)  # not yet merged
+    # MOIT.qcp5test(OPTIMIZER, CONFIG)  # not yet merged
 end
 
 @testset "MOI Integer Linear" begin
-    MOIT.intlineartest(OPTIMIZER, CONFIG)
+    # MOIT.intlineartest(OPTIMIZER, CONFIG)
+
+    # call individual tests
+    MOIT.knapsacktest(OPTIMIZER, CONFIG)
+    MOIT.int1test(OPTIMIZER, CONFIG)
+    MOIT.int2test(OPTIMIZER, CONFIG)
+    MOIT.int3test(OPTIMIZER, CONFIG)
 end
