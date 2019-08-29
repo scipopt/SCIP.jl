7256be7c0ed30c8480095743d0c49dac2a234095
Robert Schwarz <mail@rschwarz.net>
2019-08-29 09:43:53 +0200

    use Dicts provided by MINLPTests

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -5,20 +5,11 @@ const OBJTOL = 1e-4
 const PRIMALTOL = 1e-3
 const DUALTOL = NaN  # to disable the query
 
-const TERMINATION_TARGET = Dict(
-    MINLPTests.FEASIBLE_PROBLEM => JuMP.MOI.OPTIMAL,
-    MINLPTests.INFEASIBLE_PROBLEM => JuMP.MOI.INFEASIBLE,
-)
-
-const PRIMAL_TARGET = Dict(
-    MINLPTests.FEASIBLE_PROBLEM => JuMP.MOI.FEASIBLE_POINT,
-    MINLPTests.INFEASIBLE_PROBLEM => JuMP.MOI.NO_SOLUTION,
-)
-
 MINLPTests.test_directory(
     "nlp", OPTIMIZER,
     objective_tol=OBJTOL, primal_tol=PRIMALTOL, dual_tol=DUALTOL,
-    termination_target=TERMINATION_TARGET, primal_target=PRIMAL_TARGET,
+    termination_target=MINLPTests.TERMINATION_TARGET_GLOBAL,
+    primal_target=MINLPTests.PRIMAL_TARGET_GLOBAL,
     include=[
         "005_010",
         "007_010",
@@ -27,7 +18,8 @@ MINLPTests.test_directory(
 MINLPTests.test_directory(
     "nlp-cvx", OPTIMIZER,
     objective_tol=OBJTOL, primal_tol=PRIMALTOL, dual_tol=DUALTOL,
-    termination_target=TERMINATION_TARGET, primal_target=PRIMAL_TARGET,
+    termination_target=MINLPTests.TERMINATION_TARGET_GLOBAL,
+    primal_target=MINLPTests.PRIMAL_TARGET_GLOBAL,
     include=[
         "001_010",
         "002_010",
@@ -61,7 +53,8 @@ MINLPTests.test_directory(
 MINLPTests.test_directory(
     "nlp-mi", OPTIMIZER,
     objective_tol=OBJTOL, primal_tol=PRIMALTOL, dual_tol=DUALTOL,
-    termination_target=TERMINATION_TARGET, primal_target=PRIMAL_TARGET,
+    termination_target=MINLPTests.TERMINATION_TARGET_GLOBAL,
+    primal_target=MINLPTests.PRIMAL_TARGET_GLOBAL,
     include=[
         "005_010",
         "007_010",
