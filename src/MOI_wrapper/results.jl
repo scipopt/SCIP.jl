9322649267c93116910e7f0fbec5fbe457ea1370
Robert Schwarz <mail@rschwarz.net>
2019-10-11 13:46:25 +0200

    check result_index bounds before accessing array

--- src/MOI_wrapper/results.jl
+++ src/MOI_wrapper/results.jl
@@ -66,24 +66,28 @@ assert_after_prob(o::Optimizer) = assert_stage(o, SCIP_Stage.(3:10))
 
 function MOI.get(o::Optimizer, attr::MOI.ObjectiveValue)
     assert_solved(o)
+    MOI.check_result_index_bounds(o, attr)
     sols = unsafe_wrap(Array{Ptr{SCIP_SOL}}, SCIPgetSols(o), SCIPgetNSols(o))
     return SCIPgetSolOrigObj(o, sols[attr.result_index])
 end
 
 function MOI.get(o::Optimizer, attr::MOI.VariablePrimal, vi::VI)
     assert_solved(o)
+    MOI.check_result_index_bounds(o, attr)
     sols = unsafe_wrap(Array{Ptr{SCIP_SOL}}, SCIPgetSols(o), SCIPgetNSols(o))
     return SCIPgetSolVal(o, sols[attr.N], var(o, vi))
 end
 
 function MOI.get(o::Optimizer, attr::MOI.ConstraintPrimal, ci::CI{SVF,<:BOUNDS})
     assert_solved(o)
+    MOI.check_result_index_bounds(o, attr)
     sols = unsafe_wrap(Array{Ptr{SCIP_SOL}}, SCIPgetSols(o), SCIPgetNSols(o))
     return SCIPgetSolVal(o, sols[attr.N], var(o, VI(ci.value)))
 end
 
 function MOI.get(o::Optimizer, attr::MOI.ConstraintPrimal, ci::CI{SAF,<:BOUNDS})
     assert_solved(o)
+    MOI.check_result_index_bounds(o, attr)
     sols = unsafe_wrap(Array{Ptr{SCIP_SOL}}, SCIPgetSols(o), SCIPgetNSols(o))
     return SCIPgetActivityLinear(o, cons(o, ci), sols[attr.N])
 end
