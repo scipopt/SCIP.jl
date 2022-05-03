656566cd39dc5dc5ecec17bd2f573023d3acc1ec
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-05-03 11:25:38 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -80,12 +80,3 @@ end
 end
 
 include("MOI_conshdlr.jl")
-
-@testset "SCIP_CALL printing" begin
-    f() = SCIP.SCIP_OKAY
-    g(args...) = SCIP.SCIP_ERROR
-    SCIP.@SCIP_CALL f()
-    h() = SCIP.@SCIP_CALL(g(1, 2))
-    @test_throws ErrorException("g() yielded SCIP code SCIP_ERROR") SCIP.@SCIP_CALL g()
-    @test_throws ErrorException("g(1, 2) yielded SCIP code SCIP_ERROR") h()
-end
