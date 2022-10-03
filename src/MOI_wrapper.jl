08a6bf46175d2927d2b3a2097439caf36813e30b
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-10-03 18:23:35 +0200

    fix constructor

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -198,7 +198,7 @@ function MOI.empty!(o::Optimizer)
     @SCIP_CALL SCIPincludeDefaultPlugins(scip[])
     @SCIP_CALL SCIP.SCIPcreateProbBasic(scip[], "")
     # create a new problem
-    o.inner = SCIPData(scip, Dict(), Dict(), 0, 0, Dict(), Dict(), Dict(), [])
+    o.inner = SCIPData(scip, Dict(), Dict(), 0, 0, Dict(), Dict(), Dict(), Dict(), [])
     # reapply parameters
     for pair in o.params
         set_parameter(o.inner, pair.first, pair.second)
