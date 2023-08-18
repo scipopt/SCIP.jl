a28a34a0e94dfcd8a4ce3cc1b543bcda220282d9
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-08-18 11:19:30 +0900

    Update SCIP.jl to support Ipopt precompiled with LBT

--- src/SCIP.jl
+++ src/SCIP.jl
@@ -1,8 +1,19 @@
 module SCIP
 
+import LinearAlgebra
+import OpenBLAS32_jll
+
+function __init__()
+    if VERSION >= v"1.9"
+        config = LinearAlgebra.BLAS.lbt_get_config()
+        if !any(lib -> lib.interface == :lp64, config.loaded_libs)
+            LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
+        end
+    end
+end
+
 # assorted utility functions
 include("util.jl")
-
 
 # load deps, version check
 include("init.jl")
