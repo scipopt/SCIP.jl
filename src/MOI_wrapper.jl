49a6b26154cc9df014950d5be76118441c4b9f33
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-09-07 23:40:02 +0200

    added conflict support

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -39,6 +39,7 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     moi_heuristic::Any # ::Union{HeuristicCb, Nothing}
     objective_sense::Union{Nothing,MOI.OptimizationSense}
     objective_function_set::Bool
+    conflict_status::MOI.ConflictStatusCode
 
     function Optimizer(; kwargs...)
         scip = Ref{Ptr{SCIP_}}(C_NULL)
@@ -73,6 +74,7 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
             nothing,
             nothing,
             false,
+            MOI.COMPUTE_CONFLICT_NOT_CALLED,
         )
         finalizer(free_scip, o)
 
@@ -281,6 +283,7 @@ function MOI.empty!(o::Optimizer)
     end
     o.objective_sense = nothing
     o.objective_function_set = false
+    o.conflict_computed = MOI.COMPUTE_CONFLICT_NOT_CALLED
     o.moi_separator = nothing
     o.moi_heuristic = nothing
     return nothing
@@ -418,6 +421,53 @@ function MOI.get(::Optimizer, ::MOI.ListOfOptimizerAttributesSet)
         push!(attributes, MOI.TimeLimitSec())
     end
     return attributes
+end
+
+function MOI.compute_conflict!(o::Optimizer)
+    if o.conflict_status != MOI.COMPUTE_CONFLICT_NOT_CALLED
+        error("Conflict computation is destructive for the model and cannot be called twice.")
+    end
+    # free the transformed problem first
+    if LibSCIP.SCIPgetStage(o) != LibSCIP.SCIP_STAGE_PROBLEM
+        @SCIP_CALL LibSCIP.SCIPfreeTransform(o)
+    end
+    success = Ref{LibSCIP.SCIP_Bool}(333)
+    @SCIP_CALL LibSCIP.SCIPtransformMinUC(o, success)
+    if success[] != SCIP.TRUE
+        error("Failed to compute the minimum unsatisfied constraints system.\nSome constraint types may not support the required transformations")
+    end
+    MOI.optimize!(o)
+    st = MOI.get(o, MOI.TerminationStatus())
+    if st != MOI.OPTIMAL
+        error("Unexpected status $st when computing conflicts")
+    end
+    o.conflict_status = if MOI.get(o, MOI.ObjectiveValue()) > 0
+        MOI.CONFLICT_FOUND
+    else
+        MOI.NO_CONFLICT_EXISTS
+    end
+    return
+end
+
+MOI.get(o::Optimizer, ::MOI.ConflictStatus) = o.conflict_status
+
+function MOI.get(o::Optimizer, ::MOI.ConstraintConflictStatus, index::MOI.ConstraintIndex{MOI.VariableIndex})
+    o.conflict_status == MOI.CONFLICT_FOUND || error("no conflict")
+    # we cannot determine whether variable constraint (integer, binary, variable bounds) participate
+    return MOI.MAYBE_IN_CONFLICT
+end
+
+function MOI.get(o::Optimizer, ::MOI.ConstraintConflictStatus, index::MOI.ConstraintIndex)
+    o.conflict_status == MOI.CONFLICT_FOUND || error("no conflict")
+    c_name = MOI.get(o, MOI.ConstraintName(), index)
+    slack_name = "$(c_name)_master"
+    ptr = SCIPfindVar(o, slack_name)
+    if ptr == C_NULL
+        error("No constraint name corresponds to the index $index - name $c_name")
+    end
+    sol = SCIPgetBestSol(o)
+    slack_value = SCIPgetSolVal(o, sol, ptr)
+    return slack_value > 0.5 ? MOI.IN_CONFLICT : MOI.NOT_IN_CONFLICT
 end
 
 include(joinpath("MOI_wrapper", "variable.jl"))
