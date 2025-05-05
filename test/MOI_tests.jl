9a196e964896536f8fcc16078ed790f1405b8989
odow <o.dowson@gmail.com>
2025-05-05 12:07:02 +1200

    Update

--- test/MOI_tests.jl
+++ test/MOI_tests.jl
@@ -1374,6 +1374,189 @@ function test_cutting_another_optimal_solution()
     return
 end
 
+# Test, whether the callback of the separator is called.
+function test_DummySepa_no_separation()
+    # create an empty problem
+    optimizer = SCIP.Optimizer()
+    inner = optimizer.inner
+    sepa_set_scip_parameters((par, val) -> SCIP.set_parameter(inner, par, val))
+    # add variables
+    x, y = MOI.add_variables(optimizer, 2)
+    MOI.add_constraint(optimizer, x, MOI.ZeroOne())
+    MOI.add_constraint(optimizer, y, MOI.ZeroOne())
+    # add constraint: x + y ≤ 1.5
+    MOI.add_constraint(
+        optimizer,
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+        MOI.LessThan(1.5),
+    )
+    # maximize x + y
+    MOI.set(
+        optimizer,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+    )
+    MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    # add the separator
+    sepa = DummySepa.Sepa()
+    SCIP.include_sepa(inner.scip[], inner.sepas, sepa)
+    # solve the problem
+    MOI.optimize!(optimizer)
+    # the separator is called
+    @test sepa.called >= 1
+    return
+end
+
+# Test, whether adding cuts in `exec_lp` via `add_cut_sepa` works [1/2].
+function test_AddSingleCut_cut_off_one_optimal_solution()
+    atol, rtol = 1e-6, 1e-6
+    # create an empty problem
+    optimizer = SCIP.Optimizer()
+    inner = optimizer.inner
+    sepa_set_scip_parameters((par, val) -> SCIP.set_parameter(inner, par, val))
+    # add variables
+    x, y = MOI.add_variables(optimizer, 2)
+    MOI.add_constraint(optimizer, x, MOI.ZeroOne())
+    MOI.add_constraint(optimizer, y, MOI.ZeroOne())
+    # add constraint: x + y ≤ 1.5
+    MOI.add_constraint(
+        optimizer,
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+        MOI.LessThan(1.5),
+    )
+    # maximize x + y
+    MOI.set(
+        optimizer,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+    )
+    MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    # add the separator
+    varrefs = [SCIP.VarRef(x.value)]
+    coefs = [1.0]
+    sepa = AddSingleCut.Sepa(inner, varrefs, coefs, 0.0, 0.0)
+    SCIP.include_sepa(inner.scip[], inner.sepas, sepa)
+    # solve the problem
+    MOI.optimize!(optimizer)
+    # SCIP found the single remaining optimal solution
+    @test MOI.get(optimizer, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test MOI.get(optimizer, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+    @test MOI.get(optimizer, MOI.ObjectiveValue()) ≈ 1.0 atol = atol rtol = rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), x) ≈ 0.0 atol = atol rtol =
+        rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), y) ≈ 1.0 atol = atol rtol =
+        rtol
+    return
+end
+
+# Test, whether adding cuts in `exec_lp` via `add_cut_sepa` works [2/2].
+function test_AddSingleCut_cut_off_another_optimal_solution()
+    atol, rtol = 1e-6, 1e-6
+    # create an empty problem
+    optimizer = SCIP.Optimizer()
+    inner = optimizer.inner
+    sepa_set_scip_parameters((par, val) -> SCIP.set_parameter(inner, par, val))
+    # add variables
+    x, y = MOI.add_variables(optimizer, 2)
+    MOI.add_constraint(optimizer, x, MOI.ZeroOne())
+    MOI.add_constraint(optimizer, y, MOI.ZeroOne())
+    # add constraint: x + y ≤ 1.5
+    MOI.add_constraint(
+        optimizer,
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+        MOI.LessThan(1.5),
+    )
+    # maximize x + y
+    MOI.set(
+        optimizer,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+    )
+    MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    # add the separator
+    varrefs = [SCIP.VarRef(y.value)]
+    coefs = [1.0]
+    sepa = AddSingleCut.Sepa(inner, varrefs, coefs, 0.0, 0.0)
+    SCIP.include_sepa(inner.scip[], inner.sepas, sepa)
+    # solve the problem
+    MOI.optimize!(optimizer)
+    # SCIP found the single remaining optimal solution
+    @test MOI.get(optimizer, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test MOI.get(optimizer, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+    @test MOI.get(optimizer, MOI.ObjectiveValue()) ≈ 1.0 atol = atol rtol = rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), x) ≈ 1.0 atol = atol rtol =
+        rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), y) ≈ 0.0 atol = atol rtol =
+        rtol
+    return
+end
+
+# Test, whether we can cut the optimal solution.
+function test_AddSingleCut_too_strong_cut()
+    atol, rtol = 1e-6, 1e-6
+    # create an empty problem
+    optimizer = SCIP.Optimizer()
+    inner = optimizer.inner
+    sepa_set_scip_parameters((par, val) -> SCIP.set_parameter(inner, par, val))
+    # add variables
+    x, y = MOI.add_variables(optimizer, 2)
+    MOI.add_constraint(optimizer, x, MOI.ZeroOne())
+    MOI.add_constraint(optimizer, y, MOI.ZeroOne())
+    # add constraint: x + y ≤ 1.5
+    MOI.add_constraint(
+        optimizer,
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+        MOI.LessThan(1.5),
+    )
+    # maximize x + y
+    MOI.set(
+        optimizer,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.([1.0, 1.0], [x, y]),
+            0.0,
+        ),
+    )
+    MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    # add the separator
+    varrefs = [SCIP.VarRef(x.value), SCIP.VarRef(y.value)]
+    coefs = [1.0, 1.0]
+    sepa = AddSingleCut.Sepa(inner, varrefs, coefs, 0.0, 0.0)
+    SCIP.include_sepa(inner.scip[], inner.sepas, sepa)
+    # solve the problem
+    MOI.optimize!(optimizer)
+    # SCIP found the non-optimal solution, that remains after the cut.
+    @test MOI.get(optimizer, MOI.TerminationStatus()) == MOI.OPTIMAL
+    @test MOI.get(optimizer, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+    @test MOI.get(optimizer, MOI.ObjectiveValue()) ≈ 0.0 atol = atol rtol = rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), x) ≈ 0.0 atol = atol rtol =
+        rtol
+    @test MOI.get(optimizer, MOI.VariablePrimal(), y) ≈ 0.0 atol = atol rtol =
+        rtol
+    return
+end
+
 end  # module TestMOIWrapper
 
 TestMOIWrapper.runtests()
