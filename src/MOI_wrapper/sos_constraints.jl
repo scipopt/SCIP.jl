09d9d4117e4f5340bf4c30af195905013bc8adcd
odow <o.dowson@gmail.com>
2025-05-05 15:31:56 +1200

    Various refactoring and tidying of src/MOI_wrapper/sos_constraints.jl

--- src/MOI_wrapper/sos_constraints.jl
+++ src/MOI_wrapper/sos_constraints.jl
@@ -3,14 +3,14 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-# Special-Ordered-Set type 1
+# MOI.VectorOfVariables -in- MOI.SOS1{Float64}
 
 function MOI.supports_constraint(
     o::Optimizer,
     ::Type{MOI.VectorOfVariables},
     ::Type{MOI.SOS1{Float64}},
 )
-    true
+    return true
 end
 
 function MOI.add_constraint(
@@ -19,7 +19,6 @@ function MOI.add_constraint(
     set::MOI.SOS1{Float64},
 )
     allow_modification(o)
-
     varrefs = [VarRef(vi.value) for vi in func.variables]
     cr = add_special_ordered_set_type1(o.inner, varrefs, set.weights)
     ci = MOI.ConstraintIndex{MOI.VectorOfVariables,MOI.SOS1{Float64}}(cr.val)
@@ -35,11 +34,10 @@ function MOI.get(
 )
     _throw_if_invalid(o, ci)
     c = cons(o, ci)
-    nvars::Int = SCIPgetNVarsSOS1(o, c)
+    nvars = SCIPgetNVarsSOS1(o, c)
     vars = unsafe_wrap(Array{Ptr{SCIP_VAR}}, SCIPgetVarsSOS1(o, c), nvars)
-    return MOI.VectorOfVariables([
-        MOI.VariableIndex(ref(o, v).val) for v in vars
-    ])
+    x = [MOI.VariableIndex(ref(o, v).val) for v in vars]
+    return MOI.VectorOfVariables(x)
 end
 
 function MOI.get(
@@ -49,19 +47,19 @@ function MOI.get(
 )
     _throw_if_invalid(o, ci)
     c = cons(o, ci)
-    nvars::Int = SCIPgetNVarsSOS1(o, c)
+    nvars = SCIPgetNVarsSOS1(o, c)
     weights = unsafe_wrap(Array{Float64}, SCIPgetWeightsSOS1(o, c), nvars)
     return MOI.SOS1{Float64}(weights)
 end
 
-# Special-Ordered-Set type 2
+# MOI.VectorOfVariables -in- MOI.SOS2{Float64}
 
 function MOI.supports_constraint(
     o::Optimizer,
     ::Type{MOI.VectorOfVariables},
     ::Type{MOI.SOS2{Float64}},
 )
-    true
+    return true
 end
 
 function MOI.add_constraint(
@@ -70,7 +68,6 @@ function MOI.add_constraint(
     set::MOI.SOS2{Float64},
 )
     allow_modification(o)
-
     varrefs = [VarRef(vi.value) for vi in func.variables]
     cr = add_special_ordered_set_type2(o.inner, varrefs, set.weights)
     ci = MOI.ConstraintIndex{MOI.VectorOfVariables,MOI.SOS2{Float64}}(cr.val)
@@ -86,11 +83,10 @@ function MOI.get(
 )
     _throw_if_invalid(o, ci)
     c = cons(o, ci)
-    nvars::Int = SCIPgetNVarsSOS2(o, c)
+    nvars = SCIPgetNVarsSOS2(o, c)
     vars = unsafe_wrap(Array{Ptr{SCIP_VAR}}, SCIPgetVarsSOS2(o, c), nvars)
-    return MOI.VectorOfVariables([
-        MOI.VariableIndex(ref(o, v).val) for v in vars
-    ])
+    x = [MOI.VariableIndex(ref(o, v).val) for v in vars]
+    return MOI.VectorOfVariables(x)
 end
 
 function MOI.get(
@@ -100,7 +96,7 @@ function MOI.get(
 )
     _throw_if_invalid(o, ci)
     c = cons(o, ci)
-    nvars::Int = SCIPgetNVarsSOS2(o, c)
+    nvars = SCIPgetNVarsSOS2(o, c)
     weights = unsafe_wrap(Array{Float64}, SCIPgetWeightsSOS2(o, c), nvars)
     return MOI.SOS2{Float64}(weights)
 end
